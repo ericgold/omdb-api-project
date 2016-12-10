be6fdaa931385739bfc698d429ea31a56342f687
ericgold <egold05@gmail.com>
2016-12-10 11:50:31 -0800

    header content and rotten tomatoes data

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-var button = document.getElementById("button");
-//var title = document.getElementById("title").value;
+var buffButton = document.getElementById("buff-button");
 var results = document.getElementById("results");
 
-/*
-function loadJson() {
-	var movie = JSON.parse(this.responseText);
-	var shortPlot = movie.plot;
-
-	console.log(shortPlot);
-	//results.innerHTML = shortPlot;
-}
-*/
 
 function getJson() {
 	var title = document.getElementById("title").value;
-	console.log(title);
-	var query = "http://www.omdbapi.com/?plot=full&t=";
+	
+	var query = "http://www.omdbapi.com/?plot=full&type=movie&tomatoes=true&t=";
 	query += title;
-	console.log(query);
+	
 
 	var textRequest = new XMLHttpRequest();
 	textRequest.addEventListener("load", function(){
 		var movie = JSON.parse(this.responseText);
 		console.log(movie);
-		
 		var shortPlot = movie.Plot;
 		results.innerHTML = shortPlot;
+
 	});
 	textRequest.open("GET", query);
 	textRequest.send();
 }
 
-button.addEventListener("click", getJson);
+buffButton.addEventListener("click", getJson);
