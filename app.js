61635ed83ed7f8e7cbb5dce28bd20d125ccfd396
ericgold <egold05@gmail.com>
2016-12-10 20:01:41 -0800

    styles and more data

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var buffButton = document.getElementById("buff-button");
 var directorBox = document.getElementById("director");
 var castBox = document.getElementById("cast");
 var releaseDateBox = document.getElementById("release-date");
+var runtimeBox = document.getElementById("runtime");
+var awardsBox = document.getElementById("awards");
 var plotBox = document.getElementById("plot");
 var opinionBox = document.getElementById("opinion");
 
@@ -11,30 +13,49 @@ function getJson() {
 	
 	var query = "http://www.omdbapi.com/?plot=full&type=movie&tomatoes=true&t=";
 	query += title;
-	
 
 	var textRequest = new XMLHttpRequest();
-	
+
 	textRequest.addEventListener("load", function(){
 		var movie = JSON.parse(this.responseText);
-		console.log(movie);
+		//console.log(movie);
 
 		var director = movie.Director;
 		var cast = movie.Actors;
 		var release = movie.Released;
+		var runtime = movie.Runtime;
+		var awards = movie.Awards;
 		var fullPlot = movie.Plot;
 		var opinion = movie.tomatoConsensus;
 
-		directorBox.innerHTML = director;
-		castBox.innerHTML = cast;
-		releaseDateBox.innerHTML = release;
-		plotBox.innerHTML = fullPlot;
-		opinionBox.innerHTML = opinion;
+		if (opinion = "N/A") {
+			opinion = "You're on your own, bub."
+		}
+
+		clearBoxes();
+
+		directorBox.append(director);
+		castBox.append(cast);
+		releaseDateBox.append(release);
+		runtimeBox.append(runtime);
+		awardsBox.append(awards);
+		plotBox.append(fullPlot);
+		opinionBox.append(opinion);
 
 
 	});
 	textRequest.open("GET", query);
 	textRequest.send();
+
+	function clearBoxes() {
+		directorBox.innerHTML = "";
+		castBox.innerHTML = "";
+		releaseDateBox.innerHTML = "";
+		runtimeBox.innerHTML = "";
+		awardsBox.innerHTML = "";
+		plotBox.innerHTML = "";
+		opinionBox.innerHTML = "";
+	}
 }
 
 buffButton.addEventListener("click", getJson);
