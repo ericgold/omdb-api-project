13d4c997320747d0733ab01aeb3a2f1dbf3f5143
ericgold <egold05@gmail.com>
2016-12-11 16:41:55 -0800

    module pattern

--- app.js
+++ app.js
@@ -7,55 +7,61 @@ var awardsBox = document.getElementById("awards");
 var plotBox = document.getElementById("plot");
 var opinionBox = document.getElementById("opinion");
 
+(function(exports){
 
-function getJson() {
-	var title = document.getElementById("title").value;
+	exports.getJson = function() {
+		var title = document.getElementById("title").value;
 	
-	var query = "http://www.omdbapi.com/?plot=full&type=movie&tomatoes=true&t=";
-	query += title;
+		var query = "http://www.omdbapi.com/?plot=full&type=movie&tomatoes=true&t=";
+		query += title;
 
-	var textRequest = new XMLHttpRequest();
+		var textRequest = new XMLHttpRequest();
 
-	textRequest.addEventListener("load", function(){
-		var movie = JSON.parse(this.responseText);
-		//console.log(movie);
+		textRequest.addEventListener("load", function(){
+			var movie = JSON.parse(this.responseText);
+			//console.log(movie);
 
-		var director = movie.Director;
-		var cast = movie.Actors;
-		var release = movie.Released;
-		var runtime = movie.Runtime;
-		var awards = movie.Awards;
-		var fullPlot = movie.Plot;
-		var opinion = movie.tomatoConsensus;
+			var director = movie.Director;
+			var cast = movie.Actors;
+			var release = movie.Released;
+			var runtime = movie.Runtime;
+			var awards = movie.Awards;
+			var fullPlot = movie.Plot;
+			var opinion = movie.tomatoConsensus;
 
-		if (opinion = "N/A") {
-			opinion = "You're on your own, bub."
+			if (opinion = "N/A") {
+				opinion = "You're on your own, bub."
+			}
+
+			clearBoxes();
+
+			directorBox.append(director);
+			castBox.append(cast);
+			releaseDateBox.append(release);
+			runtimeBox.append(runtime);
+			awardsBox.append(awards);
+			plotBox.append(fullPlot);
+			opinionBox.append(opinion);
+
+		});
+
+		textRequest.open("GET", query);
+		textRequest.send();
+
+		function clearBoxes() {
+			directorBox.innerHTML = "";
+			castBox.innerHTML = "";
+			releaseDateBox.innerHTML = "";
+			runtimeBox.innerHTML = "";
+			awardsBox.innerHTML = "";
+			plotBox.innerHTML = "";
+			opinionBox.innerHTML = "";
 		}
-
-		clearBoxes();
-
-		directorBox.append(director);
-		castBox.append(cast);
-		releaseDateBox.append(release);
-		runtimeBox.append(runtime);
-		awardsBox.append(awards);
-		plotBox.append(fullPlot);
-		opinionBox.append(opinion);
-
-
-	});
-	textRequest.open("GET", query);
-	textRequest.send();
-
-	function clearBoxes() {
-		directorBox.innerHTML = "";
-		castBox.innerHTML = "";
-		releaseDateBox.innerHTML = "";
-		runtimeBox.innerHTML = "";
-		awardsBox.innerHTML = "";
-		plotBox.innerHTML = "";
-		opinionBox.innerHTML = "";
-	}
 }
 
-buffButton.addEventListener("click", getJson);
+}(typeof window === 'undefined' || window === null ? global.movieBuff = {} : window.movieBuff = {}))
+
+
+buffButton.addEventListener("click", movieBuff.getJson);
+
+
